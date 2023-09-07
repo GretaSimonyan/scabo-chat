594f651f7c49dfe1cb31a15ec0b76e87b1812f5f
GretaSimonyan <gsimonyan228@gmail.com>
2023-09-07 23:47:54 +0400

    integrated redux toolkit and feature sliced design

---
+++ src/app/App.tsx
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./store.ts";
+import viteLogo from '/vite.svg';
+import './styles/App.css';
+import { decrement, increment } from '../features/counter/counterSlice';
+function App() {
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <div className="card">
+        <button onClick={() => dispatch(increment())}>
+          count is {count}
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
