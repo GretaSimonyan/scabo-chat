720e1c43985facfe489f677d948a0305639b83b5
GretaSimonyan <gsimonyan228@gmail.com>
2023-09-27 22:45:26 +0400

    add Text component

---
+++ src/shared/ui/Text/Text.tsx
@@ -0,0 +1,76 @@
+import { memo } from 'react';
+
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
+
+import cls from './Text.module.scss';
+
+export enum TextTheme {
+  PRIMARY = 'primary',
+  ERROR = 'error',
+  INVERTED = 'inverted',
+}
+
+interface TextProps {
+  className?: string;
+  title?: string;
+  text?: string;
+  theme?: TextTheme;
+  align?: TextAlign;
+  size?: TextSize;
+  'data-testid'?: string;
+}
+
+export enum TextAlign {
+  RIGHT = 'right',
+  LEFT = 'left',
+  CENTER = 'center',
+}
+
+export enum TextSize {
+  S = 'size_s',
+  M = 'size_m',
+  L = 'size_l',
+}
+
+type HeaderTagType = 'h1' | 'h2' | 'h3';
+
+const mapSizeToHeaderTag: Record<TextSize, HeaderTagType> = {
+  [TextSize.S]: 'h3',
+  [TextSize.M]: 'h2',
+  [TextSize.L]: 'h1',
+};
+
+export const Text = memo((props: TextProps) => {
+  const {
+    className,
+    text,
+    title,
+    theme = TextTheme.PRIMARY,
+    align = TextAlign.LEFT,
+    size = TextSize.M,
+    'data-testid': dataTestId = 'Text',
+  } = props;
+
+  const mods: Mods = {
+    [cls[theme]]: true,
+    [cls[align]]: true,
+    [cls[size]]: true,
+  };
+
+  const HeaderTag = mapSizeToHeaderTag[size];
+
+  return (
+    <div className={classNames(cls.Text, mods, [className])}>
+      {title && (
+        <HeaderTag className={cls.title} data-testid={`${dataTestId}.Header`}>
+          {title}
+        </HeaderTag>
+      )}
+      {text && (
+        <p className={cls.text} data-testid={`${dataTestId}.Paragraph`}>
+          {text}
+        </p>
+      )}
+    </div>
+  );
+});
